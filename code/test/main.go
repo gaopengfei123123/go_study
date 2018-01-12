eac874f2934d6dce18c51f016cf0bc86b4446425
GPF <5173180@qq.com>
2018-01-12 20:54:40 +0800

    测试频道

--- code/test/main.go
+++ code/test/main.go
@@ -1,50 +1,111 @@
 package main
 import (
-    "log"
-    "strings"
-
-    "github.com/astaxie/beego/validation"
+	"fmt"
+	"time"
+    "github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql" // import your used driver
 )
 
-// 验证函数写在 "valid" tag 的标签里
-// 各个函数之间用分号 ";" 分隔，分号后面可以有空格
-// 参数用括号 "()" 括起来，多个参数之间用逗号 "," 分开，逗号后面可以有空格
-// 正则函数(Match)的匹配模式用两斜杠 "/" 括起来
-// 各个函数的结果的 key 值为字段名.验证函数名
-type user struct {
-    Id     int
-    Name   string `valid:"Required;Match(/^Bee.*/)"` // Name 不能为空并且以 Bee 开头
-    Age    int    `valid:"Range(1, 140)"` // 1 <= Age <= 140，超出此范围即为不合法
-    Email  string `valid:"Email; MaxSize(100)"` // Email 字段需要符合邮箱格式，并且最大长度不能大于 100 个字符
-    Mobile string `valid:"Mobile"` // Mobile 必须为正确的手机号
-    IP     string `valid:"IP"` // IP 必须为一个正确的 IPv4 地址
+const CONNECTION = "default"
+
+// Role 表结构
+type UserRoles struct {
+	Id int
+	Role string
+	Title string
+    Uid int
+    User *User  `orm:"rel(fk)"`
 }
 
-// 如果你的 struct 实现了接口 validation.ValidFormer
-// 当 StructTag 中的测试都成功时，将会执行 Valid 函数进行自定义验证
-func (u *user) Valid(v *validation.Validation) {
-    if strings.Index(u.Name, "admin") != -1 {
-        // 通过 SetError 设置 Name 的错误信息，HasErrors 将会返回 true
-        v.SetError("Name", "名称里不能含有 admin")
-    }
+// User 表结构
+type User struct {
+	Id   int	
+	Name string `orm:"size(100)"`
+	Password string
+	Token string `orm:"size(64)"`
+	UserRoles  []*UserRoles `orm:"reverse(many)"`
+}
+
+
+
+// UserModal 用户表模型
+type UserModal struct {
+    User
+	IsError bool
+	Error string
+	Ip string
+}
+
+func init(){
+    orm.RegisterDriver("mysql", orm.DRMySQL)
+    // set default database
+    orm.RegisterDataBase("default", "mysql", "root:123123@tcp(127.0.0.1:3306)/go?charset=utf8", 30)
+	orm.RegisterModel(new(User),new(UserRoles))
 }
 
 func main() {
-    valid := validation.Validation{}
-    u := user{Name: "", Age: 2, Email: "dev@beego.me"}
-    b, err := valid.Valid(&u)
-    if err != nil {
-        // handle error
-    }
-    var errorList []string
-    if !b {
-        // validation does not pass
-        // blabla...
-        for _, err := range valid.Errors {
-            errorList = append(errorList,err.Key)
-            log.Println(err.Key, err.Message)
-        }
-    }
+    user := UserModal{}
+    user.User.Id = 3
+    data := user.GetOne("Id")
 
-    log.Println(errorList)
+	fmt.Println("export result:",data)
+	
+
+	fmt.Println("goruntime begin")
+	syncChan1 := make(chan struct{},1)
+	syncChan2 := make(chan struct{},2)
+	go receive(strChan,syncChan1,syncChan2)
+	go send(strChan,syncChan1,syncChan2)
+	<-syncChan1
+	<-syncChan2
+	fmt.Println("goruntime end")
+
+}
+
+var strChan = make(chan string,3)
+func receive(strChan <-chan string,syncChan1 chan<- struct{},syncChan2 chan<- struct{}) {
+	syncChan1<- struct{}{}
+	fmt.Println("Received a sync signal and wait a second... [receiver]")
+	time.Sleep(time.Second)
+	for elem := range strChan {
+		fmt.Println("Received:", elem, "[receiver]")
+	}
+	fmt.Println("Stopped. [receiver]")
+	syncChan2 <- struct{}{}
+}
+
+func send (strChan chan<- string,syncChan1 chan<- struct{},syncChan2 chan<- struct{}) {
+	for _, elem := range []string{"a","b","c","d"} {
+		strChan <- elem
+		fmt.Println("Sent:", elem, "[sender]")
+		if elem == "c" {
+			syncChan1 <- struct{}{}
+		}
+	}
+	fmt.Println("wait 2 seconds.. [sender]")
+	time.Sleep(time.Second * 2)
+	close(strChan)
+	syncChan2 <- struct{}{}
+
+}
+
+
+
+// GetOne 获取一条用户信息
+func (th *UserModal) GetOne(column string) (*UserModal){
+	db := orm.NewOrm()
+    db.Using(CONNECTION)
+
+    err := db.Read(&th.User,column)
+   
+    fmt.Println("reverse:",err)
+
+	if err == orm.ErrNoRows {
+		th.Error = "no rows"
+		th.IsError = true
+	} else if err == orm.ErrMissPK {
+		th.IsError = true
+	} 
+
+	return th
 }
