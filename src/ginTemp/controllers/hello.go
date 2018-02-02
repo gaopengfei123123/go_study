5600fd670b64f2ab4f5a6399eb53903c3e4e2f1c
GPF <5173180@qq.com>
2018-02-02 16:27:31 +0800

    添加相应注释

--- src/ginTemp/controllers/hello.go
+++ src/ginTemp/controllers/hello.go
@@ -4,23 +4,27 @@ import (
 	// "fmt"
 	// "net/http"
 )
+// HelloPage 基本示例,返回 json 格式内容
 func HelloPage(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "hahaha",
 	})
 }
-
+// HelloParam 读取路由中带来的参数
 func HelloParam(c * gin.Context){
 	name := c.Param("name")
 	c.String(200, "参数为 %s", name)
 }
 
+// LoginForm 在标签中就是映射要读取的字段,同时可以进行字段验证
 type LoginForm struct {
 	User     string `form:"user" json:"user" binding:"required"`
     Password string `form:"password" json:"password" binding:"required"`
 }
 
-// HelloJson 将form表单提绑定到 struct 上,form-data
+// HelloForm 将form表单提绑定到 struct 上,form-data
+// 这里有 Bind, BindJSON, BindQuery 和 ShouldBind, ShouldBindJSON, ShouldBindQuery 这6中绑定方法
+// 加上 should 就是根据 content-type 一定的自我判定能力,代价就是损耗一些性能
 func HelloForm(c *gin.Context){
 	var json LoginForm
 	 // binding JSON,本质是将request中的Body中的数据按照JSON格式解析到json变量中
--- src/ginTemp/router/router.go
+++ src/ginTemp/router/router.go
@@ -9,14 +9,18 @@ import (
 
 // RegistRouter 注册路由
 func RegistRouter(r *gin.Engine) *gin.Engine {
-	// r.Static("/", "./view/index.html")
+	// 指定访问的静态文件
 	r.StaticFile("/", "./view/index.html")
+	// 指定访问的目录
 	r.StaticFS("/static", http.Dir("./view/static"))
+
+	// 一个正常的get请求示例
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+	// 这里引入了外部包的方法
 	r.GET("/hello", controllers.HelloPage)
 	r.GET("/hello/:name", controllers.HelloParam)
 	r.POST("/hello/login", controllers.HelloForm)
