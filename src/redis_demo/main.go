53a5c4456f29e007d790220503416fca38552baf
GPF <5173180@qq.com>
2018-03-26 19:06:41 +0800

    添加redis 包的示例

---
+++ src/redis_demo/main.go
@@ -0,0 +1,66 @@
+package main
+
+import(
+	"github.com/go-redis/redis"
+	"fmt"
+)
+// redis github 地址为: https://github.com/go-redis/redis
+
+func main(){
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+
+	err := client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+}
+
+func ExampleNewClient() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+}
+
+
+func ExampleClient() {
+	// err := client.Set("key", "value", 0).Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// val, err := client.Get("key").Result()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("key", val)
+
+	// val2, err := client.Get("key2").Result()
+	// if err == redis.Nil {
+	// 	fmt.Println("key2 does not exist")
+	// } else if err != nil {
+	// 	panic(err)
+	// } else {
+	// 	fmt.Println("key2", val2)
+	// }
+	// Output: key value
+	// key2 does not exist
+}
