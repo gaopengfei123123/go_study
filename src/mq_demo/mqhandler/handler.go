452b95b804bd93ba815351fe9b1685e6f924db18
GPF <5173180@qq.com>
2018-05-10 17:35:43 +0800

    添加 rabbitmq 的服务支持

--- src/mq_demo/mqhandler/handler.go
+++ src/mq_demo/mqhandler/handler.go
@@ -2,13 +2,15 @@ package mqhandler
 
 import(
 	// "fmt"
-	"mq_demo/mqhandler/kafka"
+	// "mq_demo/mqhandler/kafka"
+	"mq_demo/mqhandler/rabbitmq"
 )
 
 
 // MQService mq 服务,目前暂定 kafka, 包含 send 和 read 两个方法
 type MQService struct{
-	kafka.Kafka
+	// kafka.Kafka
+	rabbitmq.Rabbitmq
 }
 // MQ 队列应该实现的方法
 type MQ interface{
---
+++ src/mq_demo/mqhandler/rabbitmq/server.go
@@ -0,0 +1,108 @@
+package rabbitmq
+
+import(
+	"github.com/streadway/amqp"
+	"log"
+)
+
+// rabbitmq 的相关配置
+const(
+	RabbitmqHost = "amqp://guest:guest@localhost:5672/"
+	QueueName = "hello"
+	ConsumerName = ""
+	Durable = false
+	DeleteWhenUnused = false
+	Exclusive = false
+	NoWait = false
+	AutoAck = true
+	NoLocal = false
+)
+
+
+
+// Rabbitmq 消息队列
+type Rabbitmq struct{}
+
+// Read 向队列读取的方法
+func (rb Rabbitmq) Read(f func(jsonStr []byte)){
+	conn, err := amqp.Dial(RabbitmqHost)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		QueueName, // name
+		Durable,   // durable
+		DeleteWhenUnused,   // delete when unused
+		Exclusive,   // exclusive
+		NoWait,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, 			// queue
+		ConsumerName,     	// consumer
+		AutoAck,   			// auto-ack
+		Exclusive,  			// exclusive
+		NoLocal,  			// no-local
+		NoWait,  			// no-wait
+		nil,    			// args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			go f(d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+// Send 向队列发送的方法
+func (rb Rabbitmq) Send(key string,value string){
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := value
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	log.Printf(" [x] Sent %s", body)
+	failOnError(err, "Failed to publish a message")
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
