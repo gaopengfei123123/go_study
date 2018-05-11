145f714b5a5ec14424daa23287fe0c9d94fab79a
GPF <5173180@qq.com>
2018-05-11 15:14:46 +0800

    重新封装一层 log,添加个 uniqueid,方便日后查找

---
+++ src/logs_demo/loghandler/handler.go
@@ -0,0 +1,118 @@
+package loghandler
+
+import(
+	"github.com/astaxie/beego/logs"
+	"encoding/json"
+	"fmt"
+)
+// log 的一些设置
+const (
+	// LogPath  日志地址
+	LogPath = "./logs/rpc_ts.log"
+)
+
+// LogInfoTemplate log 信息模板
+type LogInfoTemplate struct{
+	UniqueID int64 `json:"uniqueID"`
+	Data interface{}
+}
+
+var uniqueID int64
+
+// log 的基础设置都在这里了
+func init(){
+	config := fmt.Sprintf(`{"filename":"%s"}`, LogPath )
+	logs.SetLogger(logs.AdapterFile, config)
+	// 开启文件行号显示
+	logs.EnableFuncCallDepth(true)
+	// 因为是自己封装的需要将包层级给标明,否则文件行号只会显示依赖包中的行号
+	logs.SetLogFuncCallDepth(4)
+	// 异步 chan 的大小为1k
+	logs.Async(1e3)
+
+	uniqueID = genUniqueID()
+}
+
+// 生成 uniqueID
+func genUniqueID() int64{
+	return 23333333333
+}
+
+// GetUniqueID 统一获取 uid
+func GetUniqueID() int64{
+	return uniqueID
+}
+
+// GetLogTemplate 获取 log 模板
+func GetLogTemplate() LogInfoTemplate {
+	var tmpl LogInfoTemplate
+	tmpl.UniqueID = GetUniqueID()
+	return tmpl
+}
+
+// Debug 方法
+func Debug(f interface{}){
+	tmpl := GetLogTemplate()
+	tmpl.Data = f
+	strByte, _ := json.Marshal(tmpl)
+	logs.Debug(string(strByte))
+}
+
+// Info 方法
+func Info(f interface{}){
+	tmpl := GetLogTemplate()
+	tmpl.Data = f
+	strByte, _ := json.Marshal(tmpl)
+
+	logs.Info(string(strByte))
+}
+
+// Warn 方法
+func Warn(f interface{}){
+	tmpl := GetLogTemplate()
+	tmpl.Data = f
+	strByte, _ := json.Marshal(tmpl)
+	logs.Warn(string(strByte))
+}
+
+// Error 方法
+func Error(f interface{}){
+	tmpl := GetLogTemplate()
+	tmpl.Data = f
+	strByte, _ := json.Marshal(tmpl)
+	logs.Error(string(strByte))
+}
+
+
+// Emergency 方法
+func Emergency(f interface{}){
+	tmpl := GetLogTemplate()
+	tmpl.Data = f
+	strByte, _ := json.Marshal(tmpl)
+	logs.Emergency(string(strByte))
+}
+
+// Critical 方法
+func Critical(f interface{}){
+	tmpl := GetLogTemplate()
+	tmpl.Data = f
+	strByte, _ := json.Marshal(tmpl)
+	logs.Critical(string(strByte))
+}
+
+// Alert 方法
+func Alert(f interface{}){
+	tmpl := GetLogTemplate()
+	tmpl.Data = f
+	strByte, _ := json.Marshal(tmpl)
+	logs.Alert(string(strByte))
+}
+
+// Notice 方法
+func Notice(f interface{}){
+	tmpl := GetLogTemplate()
+	tmpl.Data = f
+	strByte, _ := json.Marshal(tmpl)
+	logs.Notice(string(strByte))
+}
+
---
+++ src/logs_demo/main.go
@@ -0,0 +1,33 @@
+package main
+
+import(
+	logs "logs_demo/loghandler"
+	"fmt"
+	"time"
+)
+
+type testInfo struct{
+	Msg string `json:"msg"`
+	Code int `json:"code"`
+}
+
+func main(){
+	t1 := time.Now()
+	info := testInfo{
+		"this is msg",
+		200,
+	}
+	for i:=0;i<10;i++ {
+		logs.Debug(info)
+		logs.Info(info)
+		logs.Warn(info)
+		logs.Critical(info)
+		logs.Error(info)
+		logs.Emergency(info)
+		logs.Notice(info)
+	}
+	
+
+	elapsed := time.Since(t1)
+	fmt.Println("App elapsed: ", elapsed)
+}
